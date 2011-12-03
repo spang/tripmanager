eff7c6c10c1adfb12f7ef27c398d87d5c27fd367
Christine Spang <christine@spang.cc>
2011-12-03 17:48:37 -0500

    add session/cookie support to express invocation

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'ebi3Ial8poophoh$B1ju' });
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
